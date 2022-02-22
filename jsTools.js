93e54f2c950e27fc69faa77ed0a8d9672fcab2c8
Alireza Haji Hosseini <62589513+a1ire24@users.noreply.github.com>
2022-02-22 08:32:18 +0330

    add jsonToForm function

--- jsTools.js
+++ jsTools.js
@@ -1,13 +1,13 @@
 
-const fetchApi = function(url, data, method="POST"){
+const fetchApi = function (url, data, method="POST"){
     return new Promise((resolve, reject) => {
         const xhttp = new XMLHttpRequest();
         xhttp.open(method, url, true);
         xhttp.setRequestHeader('Content-Type', 'application/json');
         xhttp.send(JSON.stringify(data));
         xhttp.onload = function() {
-            let res = JSON.parse(xhttp.responseText);
-            resolve(res);
+            const result = JSON.parse(xhttp.responseText);
+            resolve(result);
         };
     })
 };
@@ -15,4 +15,14 @@ const fetchApi = function(url, data, method="POST"){
 const formToJson = function (formId){
     const data = new FormData(document.getElementById(formId)); 
     return Object.fromEntries(data.entries());
+}
+
+const jsonToForm = function (formId, data){
+    for ( let key in data ) {
+        try {
+            document.forms[formId].elements[key].value = data[key];
+        } catch (error) {
+            // console.log(error);
+        }
+    }
 }
