19836e4491093c4e1dff662b03eadbe9a7d90035
Alireza Haji Hosseini <a1ire24@yahoo.com>
2022-03-01 23:51:22 +0330

    add checkNationalCode

--- jsTools.js
+++ jsTools.js
@@ -42,3 +42,33 @@ const jsonToForm = function (formId, data){
         }
     }
 }
+
+const checkNationalCode = function(code) {
+    if (code === null || code === "" || code.length !== 10)
+        return false;
+    
+    if (
+        code == '0000000000' ||
+        code == '1111111111' ||
+        code == '2222222222' ||
+        code == '3333333333' ||
+        code == '4444444444' ||
+        code == '5555555555' ||
+        code == '6666666666' ||
+        code == '7777777777' ||
+        code == '8888888888' ||
+        code == '9999999999')
+        return false;
+    
+    let sum = 0;
+    for (let i = 0; i < 9; i++)
+        sum += parseInt(code[i]) * (10 - i);
+    
+    sum = (sum % 11);
+    if (sum >= 2)
+        sum = 11 - sum;
+    if (sum == code.substr(9, 1))
+        return true;
+
+    return false;          
+}
